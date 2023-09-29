54690dc3545f032588731b230cf36aadad535948
ShahidMZ <shahidmzubair@gmail.com>
2023-09-29 14:30:06 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,19 +7,21 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        // ***** Add services to the container *****
+
         builder.Services.AddControllers();
         builder.Services.AddDbContext<DataContext>(opt => 
         {
             opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
+        builder.Services.AddCors();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        app.UseHttpsRedirection();
+        // ***** Configure the HTTP request pipeline *****
 
-        app.UseAuthorization();
+        // Define the CORS policy builder
+        app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
         app.MapControllers();
 
